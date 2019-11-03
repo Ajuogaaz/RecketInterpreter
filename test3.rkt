5702ce09748b446041a80b38254ccc9b99cea284
Ajuogaaz <brianlinus1753@gmail.com>
2019-11-02 23:29:04 -0500

    updates

---
+++ test3.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname test3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(define grid
+  (λ (unit)
+    (row (stack unit))))
+
+(define row
+  (λ (unit)
+    (beside unit unit unit unit)))
+
+(define stack
+  (λ (unit)
+    (above unit unit unit unit)))
+
+
+(grid (circle 20 "outline" "blue"))
